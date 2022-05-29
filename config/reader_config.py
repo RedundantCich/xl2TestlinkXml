ee8191c9b6799d5b5af40869e5447079a515f8d0
tomasz_redundant <tombed6@gmail.com>
2022-05-29 12:31:56 +0200

    more info in config

--- config/reader_config.py
+++ config/reader_config.py
@@ -1,7 +1,12 @@
+# file format -> xlsx or xls
 file_format = 'xlsx'
 
-rows_to_skip = 2
+# rows to skip -> you should include headers here
+# For example if 1 is empty and 2 is headers, you should write in 2
+rows_to_skip = 2 
 
+# columns in use -> You can change the main keys to have them the same
+# as your excel files
 columns_in_use = {
     'B': ['testsuite', 'main_parent', 'no_additional_actions'],
     'C': ['testsuite', 'sub_parent', 'optional_test_suite_present'],
