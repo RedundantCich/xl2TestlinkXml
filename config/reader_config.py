e9f70e0ed34ec15681bb3fe1b19d6b31b53f185a
tomasz_bednarz_pl <35335768+RedundantCich@users.noreply.github.com>
2022-06-07 23:14:48 +0200

    Update reader_config.py
    Simplifying rows to skip

--- config/reader_config.py
+++ config/reader_config.py
@@ -2,8 +2,8 @@
 file_format = 'xlsx'
 
 # rows to skip -> you should include headers here
-# For example if 1 is empty and 2 is headers, you should write in 2
-rows_to_skip = 2
+# For example if 1 is empty and 2 is headers, you should write in 3
+rows_to_skip = 3
 
 # columns in use -> You can change the main keys to have them the same
 # as your excel files
