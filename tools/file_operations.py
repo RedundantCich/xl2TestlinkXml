5afbf42f9f7d688d892670f86bedd52d9ce8a71c
tomasz_bednarz_pl <35335768+RedundantCich@users.noreply.github.com>
2022-06-09 16:26:48 +0200

    took out logic from write to tree function and created a separate function for file names

--- tools/file_operations.py
+++ tools/file_operations.py
@@ -8,12 +8,16 @@ def list_files(file_format: str) -> List:
     return glob.glob(files_source)
 
 
-def write_tree_to_xml_file(file_path: str, main_test_suite: ET.Element, number_of_current_test_suite: int) -> None:
-    tree = ET.ElementTree(main_test_suite)
+def prepare_file_name(file_path: str, number_of_current_test_suite: int) -> str:
     file_name = file_path[
         file_path.rfind('\\')+1:file_path.rfind('.xl')
     ]
+    return f'{file_name}_{number_of_current_test_suite}'
+
+
+def write_tree_to_xml_file(main_element: ET.Element, file_name) -> None:
+    tree = ET.ElementTree(main_element)
     tree.write(
-        f'xml_results/{file_name}_{number_of_current_test_suite}.xml',
+        f'xml_results/{file_name}.xml',
         xml_declaration=True,
         encoding='utf-8')
